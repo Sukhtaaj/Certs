b884cdda0c8f019829968579ab87700c9df52c0a
Sukhdeep <sukhtaaj@gmail.com>
2013-07-05 12:36:40 +0530

    Delete dbase.php

--- dbase.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Database information
-$database    = 'sukhdeep';
-$dbHost      = '202.164.53.122';
-$dbUser      = 'sukhdeep';
-$dbPass      = 'odtphp';
-$connection  = mysql_connect ($dbHost, $dbUser, $dbPass);
-mysql_select_db ($database, $connection);
-?>
