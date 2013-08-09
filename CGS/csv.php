23bec7d052a5350344a6a1f661165f051b393530
Sukhdeep <sukhtaaj@gmail.com>
2013-08-09 22:50:56 +0530

    Update csv.php

--- CGS/csv.php
+++ CGS/csv.php
@@ -1,71 +1,73 @@
-<?php session_start(); ?>
-<?php
+<?php session_start();
 
 require_once('../library/odf.php');
 
-$csv = $_FILES["file"]["name"];
+  $id = uniqid();				//To be used with filenames to differentiate simultaneous files being processed 
+  $csv = $id.$_FILES["file"]["name"];
 
 /******************************** csv File input validation********************************************/
+
 //Link to other file in case any of folllowing conditions fail
-$url = "<meta http-equiv='Refresh' content='1; URL=option.php?var=csv'>";
+$url = "<meta http-equiv='Refresh' content='3; URL=option.php?var=csv'>";
 
-if($csv == NULL)		//checks if no file is selected
-{
-echo "<center><h2>NO (.csv) File Selected</h2></center>";
-echo $url;
-exit;
-}
+
+if($_FILES["file"]["name"] == NULL)		//checks if no file is selected
+	{
+	echo "<center><h2>NO (.csv) File Selected</h2></center>";
+	echo $url;
+	exit;
+	}
+
 else
-{
-$ext = strrchr($csv,".");
+	{
+	$ext = strrchr($csv,".");
 
-if($ext != ".csv")		//checks if csv format file is not selected
-{
-echo "<center><h2>Invalid File Format for .csv file...</h2></center>";
-echo $url;
-exit;
-}
-move_uploaded_file($_FILES["file"]["tmp_name"],"uploads/csv/data/".$_FILES["file"]["name"]);
-} 
+	   if($ext != ".csv")			//checks if csv format file is not selected
+		{
+		echo "<center><h2>Invalid File Format for .csv file...</h2></center>";
+		echo $url;
+		exit;
+		}
+move_uploaded_file($_FILES["file"]["tmp_name"],"uploads/csv/data/".$csv);
+	} 
 
-  $base = $_SESSION["base"];		//Getting file name with filled Institute Details
-  $odf = new odf("odt/base/$base.odt"); //Initializing the object with above file name
-
-
-  $file = $_FILES["photo"]["name"];
-  if($file == NULL)		//checks if no file is selected
-  {
-  echo "<center><h2>No compressed file selected for images<h2></center>";
-  echo $url;
-  exit;
-  }
-
-  move_uploaded_file($_FILES["photo"]["tmp_name"],"uploads/csv/images/".$_FILES["photo"]["name"]);
+$base = $_SESSION["base"];			//Getting file name with filled Institute Details
   
-  $extension = strrchr($file,".");	//using strrchr() to fetch the extension of the file
+$odf = new odf("odt/base/$base.odt"); 		//Initializing the object with above file name
   
-  chdir('uploads/csv/images');		//Changing the directory to extract the files at that location
+$file = $_FILES["photo"]["name"];
+  	if($file == NULL)			//checks if no file is selected
+  	  {
+  	    echo "<center><h2>No compressed file selected for images<h2></center>";
+  	    echo $url;
+  	    exit;
+    	  }
+chdir('uploads/csv/');
+exec("mkdir $id");				//MAking folder to store the compressed file and then ectract it.
+move_uploaded_file($_FILES["photo"]["tmp_name"],"$id/$file");     //storing the compressed folder for images in 
   
-  if ($extension == ".gz")
-    {
-      //extracting the tar.gz file;
-      exec("tar -zxvf $file");
-    }
-  elseif($new == ".zip")
-    {
-     //extracting the zip file;
-     exec("unzip $file");
-    }
-  else
-    {
-     echo "<center><strong>Invalid File Format for compressed images Folder.</strong></center>";
-     echo $url;
-     exit;      
-}
-chdir('../../..');		//changing directory back to previous 
+$extension = strrchr($file,".");		//using strrchr() to fetch the extension of the file
+  
+chdir("$id");					//Changing the directory to extract the files at that location
+  
+	if ($extension == ".gz")
+	  {
+      	    exec("tar -zxvf $file");		//extracting the tar.gz file;
+    	  }
+  	elseif($extension == ".zip")
+    	  {
+     	    exec("unzip $file");		//extracting the zip file;
+    	  }
+  	else
+  	  {
+	    echo "<center><strong>Invalid File Format for compressed images Folder.</strong></center>";
+	    echo $url;
+	    exit;      
+	  }
+chdir('../../..');				//changing directory back to previous 
 
-$dest =  strtok($file,".");	//using strtok for storing the filename without extension
-unlink("uploads/csv/images/$file");  //After Extracting Deleting the compressed file 
+$dest =  strtok($_FILES["photo"]["name"],".");	//using strtok for storing the folder Name after extraction
+unlink("uploads/csv/$id/$file");  		//After Extracting Deleting the compressed file 
 
 
 $csvfile = fopen("uploads/csv/data/$csv","r");	//Opening csv file in read mode
@@ -76,7 +78,7 @@ while($result = fgetcsv($csvfile))		//Fetching data in each row of csv file to a
 	
 		 //image
             
-                $pic = "uploads/csv/images/$dest/".$result[7];
+                $pic = "uploads/csv/$id/$dest/".$result[7];
 		if(!file_exists($pic))
                   {
                   $pic = "uploads/manual/image.gif";
@@ -89,36 +91,35 @@ while($result = fgetcsv($csvfile))		//Fetching data in each row of csv file to a
 		         $article->nameArticle(" ".$result[0]." ".$result[1]." ".$result[3]);
 		else
                          $article->nameArticle(" ".$result[0]." ".$result[1]." ".$result[2]." ".$result[3]); 
-		//department
+		//Institute/Department Name
 		if($result[5] == NULL)
 			$article->deptArticle($result[4].", ".$result[6]);
 		else
 			$article->deptArticle($result[4].", ".$result[5]);
 	
-	$article->merge();		//Ending the current segment
+	$article->merge();			//Ending the current segment
 }	
 
-$odf->mergeSegment($article);		//Ending the segment Object
+$odf->mergeSegment($article);			//Ending the segment Object
 
-$certName = uniqid();		//Using Function Uniqid() for unique name every File generated
 // We save the file
-$odf -> saveToDisk("odt/cert/$certName.odt"); 
+$odf -> saveToDisk("odt/cert/$id.odt"); 
 
 //copying the odt file to be converted to PDF
-copy("odt/cert/$certName.odt", "../odt2pdf/cde-root/home/sukhdeep/Desktop/$certName.odt");
+	copy("odt/cert/$id.odt", "../odt2pdf/cde-root/home/sukhdeep/Desktop/$id.odt");
 
 //changing Directory
-chdir('../odt2pdf/cde-root/home/sukhdeep');
+	chdir('../odt2pdf/cde-root/home/sukhdeep');
 
 //Command for conversion to PDF
-$myCommand = "./libreoffice.cde --headless --convert-to pdf:writer_pdf_Export Desktop/$certName.odt --outdir Desktop/";
-exec ($myCommand);
+	$myCommand = "./libreoffice.cde --headless --convert-to pdf:writer_pdf_Export Desktop/$id.odt --outdir Desktop/";
+	exec ($myCommand);
 
 //Copying the converted file to the PDF folder
-copy("Desktop/".str_replace(".odt", ".pdf", "$certName.odt"), "../../../../CGS/pdf/".str_replace(".odt", ".pdf", "$certName.odt"));
-//Deleting converted files after copying
-unlink("Desktop/$certName.pdf");
-unlink("Desktop/$certName.odt");
+	copy("Desktop/$id.pdf", "../../../../CGS/pdf/$id.pdf");
+	unlink("Desktop/$id.pdf");
+	unlink("Desktop/$id.odt");
+
 
 
 echo   '<html>
@@ -129,11 +130,11 @@ echo   '<html>
 	<body>
 	<center>
 	<h1>Your Certificate has been Generated!</h1>
-	<form action="odt/cert/'.$certName.'.odt">
+	<form action="odt/cert/'.$id.'.odt">
 	<input class="btn btn-primary" type="submit" value="Download ODT">
 	</form>
 
-	<form action="pdf/'.$certName.'.pdf">
+	<form action="pdf/'.$id.'.pdf">
 	<input class="btn btn-primary" type="submit" value="View/Download PDF">
 	</form>
 
